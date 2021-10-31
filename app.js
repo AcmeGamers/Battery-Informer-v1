6c764960dbc895bb675079021463b4d89951d4e1
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2021-10-31 09:28:24 +0500

    adding windows
    Gotta add material ui later

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const { app, BrowserWindow } = require("electron");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Window Name",
+    width: 500,
+    height: 500,
+  });
+}
+
+app.on("ready", createMainWindow);
