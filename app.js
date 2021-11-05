692f87c8b22ca2e285b6400bc903041cc8720ccf
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2021-11-05 07:22:39 +0500

    Adding Ignore Values

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const {
   ipcMain,
 } = require("electron");
 const AutoLaunch = require("auto-launch");
-process.env.NODE_ENV = "development"; //development
+process.env.NODE_ENV = "production"; //development
 const isDev = process.env.NODE_ENV !== "production" ? true : false,
   isMac = process.env.NODE_ENV === "darwin" ? true : false,
   isWin = process.env.NODE_ENV === "win32" ? true : false,
